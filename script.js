d8d6cc36a52131a83193bff5d1946cc13452e556
Irsa Ilyas <irsailyas5904@gmail.com>
2024-11-09 22:54:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById("toggle-skills");
+var skills = document.getElementById("skills");
+btn.addEventListener('click', function () {
+    if (skills.style.display === "none") {
+        skills.style.display = "block";
+    }
+    else {
+        skills.style.display = "none";
+    }
+});
